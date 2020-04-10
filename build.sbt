779b38254c8e9e24473743a487c15b141f5a7d5e
usaidkkb <usaid.kaukab1@gmail.com>
2020-04-10 23:02:46 +0200

    Dataframe to Dataset conversion

--- build.sbt
+++ build.sbt
@@ -2,9 +2,9 @@ name := "call-a-bike-analysis"
 
 version := "0.1"
 
-scalaVersion := "2.12.11"
+scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" % "spark-core_2.12" % "2.4.5",
-  "org.apache.spark" % "spark-sql_2.12" % "2.4.5"
+  "org.apache.spark" % "spark-core_2.11" % "2.2.0",
+  "org.apache.spark" % "spark-sql_2.11" % "2.2.0"
 )
--- src/main/scala/CallABikeAnalysis.scala
+++ src/main/scala/CallABikeAnalysis.scala
@@ -1,8 +1,10 @@
 import org.apache.spark.sql._
 
 object CallABikeAnalysis {
+  case class CityDS (city: Option[String])
   def main (args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("Call A Bike Analysis").getOrCreate()
+    import spark.implicits._
     val df =
       spark
         .read
@@ -11,12 +13,26 @@ object CallABikeAnalysis {
         .option("header", "true")
         .option("inferSchema", "true")
         .load(args(0))
+        .select($"CITY_RENTAL_ZONE".as("city"))
+        .as[CityDS]
 
-    println(topCitiesByUsage(df, spark).show(20))
+    val topCitiesByUsageDS = top20CitiesByUsage(df, spark)
+    println(topCitiesByUsageDS.show())
+
+    topCitiesByUsageDS
+      .write
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .save(args(1))
   }
 
-  def topCitiesByUsage(dataFrame: DataFrame, sparkSession: SparkSession) = {
+  def top20CitiesByUsage(dataset: Dataset[CityDS], sparkSession: SparkSession) = {
     import sparkSession.implicits._
-    dataFrame.groupBy("CITY_RENTAL_ZONE").count().sort($"count".desc)
+    dataset
+      .filter(_.city.isDefined)
+      .groupBy("city")
+      .count()
+      .sort($"count".desc)
+      .limit(20)
   }
 }
